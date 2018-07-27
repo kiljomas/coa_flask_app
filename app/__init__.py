b6c38ccaea7617fba507b68fd0873dd21bdb8883
Priyanka Samarth <priyanka.sam.22@gmail.com>
2018-07-27 15:49:15 -0400

    Testing flask-cors.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app(config_name):
     """Create an application instance"""
     application = Flask(__name__)
+    CORS(application)
 
     cfg = os.path.join(os.getcwd(), 'config', config_name + '.py')
     application.config.from_pyfile(cfg)
