3c992cacde48f138532754dde375ee68e8fcd7b5
Priyanka Samarth <priyanka.sam.22@gmail.com>
2018-09-30 19:59:38 -0400

    API to get dirty dozen.

--- app/main/routes.py
+++ app/main/routes.py
@@ -12,6 +12,11 @@ def index():
 
 @main.route('/sitecategoriesbreakdown')
 def site_details():
+    location_category = request.args.get('locationCategory', 
+                                          default = 'site', type = str)
+    site_id = request.args.get('siteId',default = 0, type = int)
+    location_name = request.args.get('locationName',
+                                 default = 'Union Beach', type = str)
     site_name = 'Union Beach'
     start_date = datetime.datetime(2016, 1 ,1)
     end_date = datetime.datetime(2016, 12, 31)
@@ -41,4 +46,34 @@ def site_list():
 
     return jsonify(site_names=json_list)
 
+
+@main.route('/dirtydozen')
+def dirty_dozens():
+    #set the values allowed for the location category
+    location_category = request.args.get('locationCategory', 
+                                          default = 'site', type = str)
+    #site_id = request.args.get('siteId',default = 0, type = int)
+    location_name = request.args.get('locationName',
+                                 default = 'Union Beach', type = str)
+
+    start_date_str = request.args.get('startDate',
+                                    default = '2016-1-1', type = str)
+
+    end_date_str = request.args.get('endDate',
+                                default = '2018-12-31', type = str)
+    start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
+    end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
+    result = CoaSummaryView.query.filter(CoaSummaryView.site_name == location_name, \
+                                           CoaSummaryView.volunteer_date > start_date,
+                                           CoaSummaryView.volunteer_date < end_date). \
+                                           with_entities(CoaSummaryView.item_name, \
+                                           db.func.sum(CoaSummaryView.quantity)). \
+                                           group_by(CoaSummaryView.item_name)
+
+    #remove the NULL item_name
+    result = filter(lambda item: item[0], result)
+    result = sorted(result, key=lambda tup:tup[1], reverse = True)
+    result = result[:12]
+    result_dict = dict((x,y) for x,y in result)
+    return jsonify(items=result_dict)
 
