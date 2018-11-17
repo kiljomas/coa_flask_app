36a85fe7d0c17cd6f23d875803457b7b696affa9
kiljomas <ssamojlik@yahoo.com>
2018-11-16 20:55:32 -0500

    Changing indentation for sqlalchemy queries

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,25 +5,6 @@ import datetime
 from .. import db
 from collections import OrderedDict
 
-LOCATION_CATEGORIES = {
-    "site": {
-        "label": "Site",
-        "column": CoaSummaryView.site_name
-    },
-    "town": {
-        "label": "Town",
-        "column": CoaSummaryView.town
-    },
-    "county": {
-        "label": "County",
-        "column": CoaSummaryView.county
-    }
-}
-
-def get_location_category_column(location_category):
-    location_category = location_category if location_category in LOCATION_CATEGORIES else "site"
-    return LOCATION_CATEGORIES[location_category]["column"]
-
 @main.route('/')
 def index():
     #for row in CoaSummaryView.query.all():
@@ -40,28 +21,31 @@ def site_details():
     site_name = 'Union Beach'
     start_date = datetime.datetime(2016, 1 ,1)
     end_date = datetime.datetime(2016, 12, 31)
-    print (site_name)
-    result = CoaSummaryView.query.filter(CoaSummaryView.site_name == site_name, \
-                                           CoaSummaryView.volunteer_date > start_date,
-                                           CoaSummaryView.volunteer_date < end_date). \
-                                           with_entities(CoaSummaryView.material, \
-                                           db.func.sum(CoaSummaryView.quantity)). \
-                                           group_by(CoaSummaryView.material)
-    for row in result:
-       print(row)
+    
+    result = CoaSummaryView.query \
+        .filter(
+            CoaSummaryView.site_name == site_name, \
+            CoaSummaryView.volunteer_date > start_date,
+            CoaSummaryView.volunteer_date < end_date) \
+        .with_entities(
+            CoaSummaryView.material, \
+            db.func.sum(CoaSummaryView.quantity)) \
+        .group_by(CoaSummaryView.material)
+
     return jsonify(result)
 
 @main.route('/getsitesdropdownlist')
 def site_list():
-    sql_result = CoaSummaryView.query.filter().\
-                                      with_entities(CoaSummaryView.site_name).\
-                                      group_by(CoaSummaryView.site_name).\
-                                      order_by(CoaSummaryView.site_name).\
-                                      all()
+    sql_result = CoaSummaryView.query \
+        .filter() \
+        .with_entities(CoaSummaryView.site_name) \
+        .group_by(CoaSummaryView.site_name) \
+        .order_by(CoaSummaryView.site_name) \
+        .all()
+
     json_list = list()
     for row in sql_result:
         if len(row) == 1:
-            print(row)
             json_list.append(row[0])
 
     return jsonify(site_names=json_list)
@@ -80,13 +64,32 @@ def create_location_dict(category, label, location_sql_result):
 def all_locations_list():
     result_list = list()
 
-    for category, category_info in LOCATION_CATEGORIES.items():
-        sql_result = CoaSummaryView.query.filter().\
-                            with_entities(category_info["column"]).\
-                            group_by(category_info["column"]).\
-                            order_by(category_info["column"]).\
-                            all()
-        result_list.append(create_location_dict(category, category_info["label"], sql_result))
+    # Get site names
+    site_name_sql_result = CoaSummaryView.query \
+        .filter() \
+        .with_entities(CoaSummaryView.site_name) \
+        .group_by(CoaSummaryView.site_name) \
+        .order_by(CoaSummaryView.site_name) \
+        .all()
+    result_list.append(create_location_dict('site','Site',site_name_sql_result))
+
+    # Get towns
+    town_sql_result = CoaSummaryView.query \
+        .filter() \
+        .with_entities(CoaSummaryView.town) \
+        .group_by(CoaSummaryView.town) \
+        .order_by(CoaSummaryView.town) \
+        .all()
+    result_list.append(create_location_dict('town','Town',town_sql_result))
+
+    # Get counties
+    county_sql_result = CoaSummaryView.query \
+        .filter() \
+        .with_entities(CoaSummaryView.county) \
+        .group_by(CoaSummaryView.county) \
+        .order_by(CoaSummaryView.county) \
+        .all()
+    result_list.append(create_location_dict('county','County',county_sql_result))
 
     return jsonify(locations=result_list)
 
@@ -102,15 +105,13 @@ def dirty_dozens():
     start_date_str = request.args.get('startDate', default = '2016-1-1', type = str)
     end_date_str = request.args.get('endDate', default = '2018-12-31', type = str)
     
-    location_category_column = get_location_category_column(location_category)
-
     # convert date strings to dates
     start_date = parse_date_string(start_date_str)
     end_date = parse_date_string(end_date_str)
 
     result = CoaSummaryView.query \
         .filter(
-            location_category_column == location_name,
+            CoaSummaryView.site_name == location_name,
             CoaSummaryView.volunteer_date >= start_date,
             CoaSummaryView.volunteer_date <= end_date) \
         .with_entities(
@@ -125,7 +126,7 @@ def dirty_dozens():
 
     total_items = CoaSummaryView.query \
         .filter(
-            location_category_column == location_name,
+            CoaSummaryView.site_name == location_name,
             CoaSummaryView.volunteer_date >= start_date,
             CoaSummaryView.volunteer_date <= end_date) \
         .with_entities(db.func.sum(CoaSummaryView.quantity)) \
@@ -143,7 +144,6 @@ def dirty_dozens():
         ))
 
     return jsonify(dirtydozen=json_list)
-
 
 @main.route('/validdaterange')
 def valid_date_range():
